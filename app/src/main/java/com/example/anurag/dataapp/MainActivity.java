6cd2673f7de64d09789c3b86af0cd3377230f7e0
agoel15006 <anurag15006@iiitd.ac.in>
2016-11-15 18:37:11 +0530

    Changes

--- app/src/main/java/com/example/anurag/dataapp/MainActivity.java
+++ app/src/main/java/com/example/anurag/dataapp/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.anurag.dataapp;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,10 +14,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -31,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private ServerTask mTask= null;
     private TextView mTextView = null;
     private ImageButton mButton = null;
-    private View mDownloadFormView;
     private String output = null;
 
     @Override
@@ -41,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = (TextView) findViewById(R.id.textview2);
         mButton = (ImageButton) findViewById(R.id.download);
-        mDownloadFormView = findViewById(R.id.download_form);
 
         if(savedInstanceState!=null) {
             output = savedInstanceState.getString("output");
@@ -63,25 +59,6 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putString("output", output);
     }
-
-/*    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mDownloadFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mDownloadFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mDownloadFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-        } else {
-            mDownloadFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }*/
 
     private class ServerTask extends AsyncTask<String, Void, String> {
 
@@ -124,11 +101,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this.context, "Text empty", Toast.LENGTH_LONG).show();
             }
             else {
-                Log.d("MainActivity**",result);
+                Log.d("MainActivity@@",result);
                 Document html = Jsoup.parse(result);
                 output = html.title();
                 mTextView.setText(output);
-                //Toast.makeText(this.context, "Download completed!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this.context, "Download completed!", Toast.LENGTH_LONG).show();
             }
         }
     }
